8da22c09a023ee8d12e1ee0f871d87d1e3de1135
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-22 09:08:54 +0200

    Done with Filter Array exercise of HackerRank

---
+++ src/main/scala/com/garciparedes/hackerrank/fp/FilterArray.scala
@@ -0,0 +1,17 @@
+package com.garciparedes.hackerrank.fp
+
+import scala.io
+
+object FilterArray extends App{
+
+    def f(delim: Int, arr: List[Int]): List[Int] = arr.filter(_ < delim)
+
+    val delim: Int = io.StdIn.readInt
+
+    val list: List[Int] = Iterator.continually(io.StdIn.readLine)
+      .takeWhile(_.nonEmpty)
+      .map(_.toInt)
+      .toList
+
+    f(delim, list).foreach(println)
+}
