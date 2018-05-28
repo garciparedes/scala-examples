49a4a59eab5386065d4010f17314936c0499b227
Sergio Garcia Prado <sergio@garciparedes.me>
2018-05-28 09:21:34 +0200

    Done with Reverse a List exercise

---
+++ src/main/scala/com/garciparedes/hackerrank/fp/ReverseAList.scala
@@ -0,0 +1,22 @@
+package com.garciparedes.hackerrank.fp
+
+import scala.annotation.tailrec
+import scala.io
+
+object ReverseAList extends App {
+
+  @tailrec
+  def f(arr: List[Int], arr2:List[Int] = List()): List[Int] = {
+    arr.length match {
+      case 0 => arr2
+      case _ => f(arr.drop(1), arr.head +: arr2)
+    }
+  }
+
+  val list: List[Int] = Iterator.continually(io.StdIn.readLine)
+    .takeWhile(_.nonEmpty)
+    .map(_.toInt)
+    .toList
+
+  f(list).foreach(println)
+}
