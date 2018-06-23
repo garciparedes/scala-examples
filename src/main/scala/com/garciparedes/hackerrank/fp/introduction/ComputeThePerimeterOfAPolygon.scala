b1be7c5aa7367f2a12cec07d693ab3e844f34ef0
Sergio Garcia Prado <sergio@garciparedes.me>
2018-06-23 12:56:17 +0200

    Minor style improvement

--- src/main/scala/com/garciparedes/hackerrank/fp/introduction/ComputeThePerimeterOfAPolygon.scala
+++ src/main/scala/com/garciparedes/hackerrank/fp/introduction/ComputeThePerimeterOfAPolygon.scala
@@ -10,12 +10,12 @@ object ComputeThePerimeterOfAPolygon extends App {
     .map(_ => read.split(' ').map(_.toInt))
     .map { case Array(f1, f2) => (f1, f2) }
 
-  val p = (points, points.drop(1) ++ List(points.head))
+  val perimeter = (points, points.drop(1) :+ points.head)
     .zipped
     .map((a, b) => {
       math.sqrt(math.pow(a._1 - b._1, 2) + math.pow(a._2 - b._2, 2))
     })
     .sum
 
-  println(p)
+  println(perimeter)
 }
